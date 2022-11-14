44805ac7e44886a40f8c7e1c9e440654cac6eff9
siraphat <siraphat975@gmail.com>
2022-11-14 20:09:36 +0700

    update appbar and searchbar

---
+++ yrs_lib_frontend/src/component/AppBar.tsx
@@ -0,0 +1,60 @@
+import React from "react"
+import school10 from "../assets/school_logo_10.png"
+import backbutton from "../assets/back_button.png"
+import "../css/homepage.css"
+
+function AppBar(){
+
+    return (
+        <div>
+            <header className="header">
+                <div>
+                    <div className="header_1">
+                        <div className="position">
+                            <div>
+                                <img 
+                                    src={school10}
+                                    className="school_image"
+                                />
+                            </div>
+                            <div className="big_box_username">
+                                <div className="box_username">
+                                    <div className=" username">
+                                        ปุริศ ขาวจันทร์
+                                    </div>                                
+                                </div>
+                                <div className="big_box_back_button">
+                                    <button className="small_box_back_button">
+                                        <img
+                                            src={backbutton}
+                                            className="back_button"
+                                        />
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className="header_2">
+                        <div className="small_header_2 action_page">
+                            <a className="page_name " >หน้าแรก</a>
+                        </div>
+                        <div className="small_header_2 action_page">
+                            <a className="page_name ">รายการยืมหนังสือ</a>
+                        </div>
+                        <div className="small_header_2 action_page">
+                            <a className="page_name">
+                                ประวัติการยืม-คืนหนังสือ
+                            </a> 
+                        </div>
+                        <div className="small_header_2">
+                        </div>
+                    </div>    
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default AppBar;
--- yrs_lib_frontend/src/page/homePage.tsx
+++ yrs_lib_frontend/src/page/homePage.tsx
@@ -1,70 +1,12 @@
-import "../css/homepage.css"
-import school10 from "../assets/school_logo_10.png"
-import backbutton from "../assets/back_button.png"
-import { useEffect } from "react"
-import Cookies from "js-cookie"
-import instanceAxios from "../../config/baseAxios"
+import SearchBar from "../component/SearchBar"
+import AppBar from "../component/AppBar"
 
 function HomePage(){
 
-    useEffect(() => {
-        console.log(Cookies.get('auth'));
-    }, [])
-
-    function logout(){
-        console.log("test");
-        instanceAxios.get("/logout").then(res => console.log(res));
-    }
-
     return (
         <div>
-            <header className="header">
-                <div>
-                    <div className="header_1">
-                        <div className="position">
-                            <div>
-                                <img 
-                                    src={school10}
-                                    className="school_image"
-                                />
-                            </div>
-                            <div className="big_box_username">
-                                <div className="box_username">
-                                    <div className=" username">
-                                        ปุริศ ขาวจันทร์
-                                    </div>                                
-                                </div>
-                                <div className="big_box_back_button">
-                                    <button className="small_box_back_button">
-                                        <img
-                                            onClick={logout} 
-                                            src={backbutton}
-                                            className="back_button"
-                                        />
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className="header_2">
-                        <div className="small_header_2 action_page">
-                            <a className="page_name " >หน้าแรก</a>
-                        </div>
-                        <div className="small_header_2 action_page">
-                            <a className="page_name ">รายการยืมหนังสือ</a>
-                        </div>
-                        <div className="small_header_2 action_page">
-                            <a className="page_name">
-                                ประวัติการยืม-คืนหนังสือ
-                            </a> 
-                        </div>
-                        <div className="small_header_2">
-                        </div>
-                    </div>    
-                </div>
-            </header>
+            <AppBar/>
+            <SearchBar/>
         </div>
     )
 }
