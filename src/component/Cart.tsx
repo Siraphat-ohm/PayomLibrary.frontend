eda758a227b45d89fa0ee5662bc52757dac69762
siraphat <siraphat975@gmail.com>
2023-01-21 19:26:28 +0700

    idk

--- src/component/Cart.tsx
+++ src/component/Cart.tsx
@@ -1,9 +1,6 @@
 import { Button, Offcanvas, Stack } from "react-bootstrap";
 import { useCart } from "../context/CartContext";
 import { CartItem } from "./CartItem";
-import io from "socket.io-client"
-
-const socket = io("http://127.0.0.1:9999")
 
 type CartProps = {
     isOpen: boolean
@@ -13,9 +10,6 @@ type CartProps = {
 export function Cart({isOpen}:CartProps) {
     const { cartItems, closeCart, cartQuantity } = useCart()
     const order = cartItems.map( item => ({id : item.id, quantity : item.quantity}))
-    const sendOrder = () => {
-        socket.emit("order",order)
-    }
     return (
         <Offcanvas show={isOpen} onHide={closeCart} placement='end'>
             <Offcanvas.Header closeButton>
@@ -27,7 +21,7 @@ export function Cart({isOpen}:CartProps) {
                         <CartItem key={item.id} {...item}/>
                     ))}
                     <span style={{ fontSize: "1rem" }}>total : {cartQuantity}</span>
-                    <Button onClick={sendOrder}>borrowed</Button>
+                    <Button >borrowed</Button>
                 </Stack>
             </Offcanvas.Body>
         </Offcanvas>
