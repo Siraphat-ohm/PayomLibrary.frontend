4a7ea9cc03ad99ffa38e2183fc5a51680e181c85
siraphat <siraphat975@gmail.com>
2023-01-22 11:33:38 +0700

    add isLoing into context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,22 +4,35 @@ type AuthProviderProps = {
     children : JSX.Element | JSX.Element[]
 }
 
-type AuthProps = boolean
+type AuthProps = {
+    accessToken? : string
+}
 
 
 type AuthContext = {
     auth : AuthProps
-    setAuth : Dispatch<SetStateAction<boolean>>
+    setAuth : Dispatch<SetStateAction<AuthProps>>
+    login : () => void
+    logout : () => void
+    isLogin : boolean
 }
 
 const AuthContext = createContext({} as AuthContext)
 
 export const AuthProvider = ({ children } : AuthProviderProps) => {
-    const [auth, setAuth] = useState(false);
-    console.log(auth);
+    const [auth, setAuth] = useState({});
+    const [ isLogin , setIsLogin ] = useState<boolean>(false)
+
+    const login = () => {
+        setIsLogin(true)
+    }
+
+    const logout = () => {
+        setIsLogin(false)
+    }
 
     return (
-        <AuthContext.Provider value={ { auth, setAuth} }>
+        <AuthContext.Provider value={ { auth, setAuth , login, logout, isLogin} }>
             {children}
         </AuthContext.Provider>
     )
