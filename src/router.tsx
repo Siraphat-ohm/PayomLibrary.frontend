40db21e2a178a7f4250b7f168245db7d4d6d5ce4
siraphat <siraphat975@gmail.com>
2023-01-22 11:32:51 +0700

    add navbar and entire chilldren to children of login

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import NnavBar from "./component/NavBar";
+import NavBar from "./component/NavBar";
 import HistoryPage from "./pages/historyPage";
 import HomePage from "./pages/homePage";
 import ListPage from "./pages/listPage";
@@ -7,28 +7,32 @@ import Login from "./pages/login";
 
 const router = createBrowserRouter([
     {
-        path:"/login",
-        element: <Login />,
-        
-    },
-    {
         path:"/",
-        element: <NnavBar/>,
+        element: <Login />,
         children : [
             {
-                path:"home",
-                element: <HomePage/>
-            },
-            {
-                path:"list",
-                element: <ListPage/>
-            },
-            {
-                path:"history",
-                element: <HistoryPage/>
+                path: "/",
+                element: <NavBar/>,
+                children: [
+                    {
+                        path:"home",
+                        element: <HomePage/>
+                    },
+                    {
+                        path:"list",
+                        element: <ListPage/>
+                    },
+                    {
+                        path:"history",
+                        element: <HistoryPage/>
+                    }
+                ]
             }
         ]
-        
+
+    },{
+        path:"*",
+        element: <h1>หาไม่เจอหรือเธอไม่มี</h1>
     }
 ])
 
