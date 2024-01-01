507fde2a0bf9e333b8d5086e030ae2ddf69b76bb
Siraphat-ohm <siraphat975@gmail.com>
2024-01-01 14:26:49 +0700

    refactor init

--- src/types/cart.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface CartItem {
-    id:number,
-    title:string,
-    imgBase64:string,
-    quantity:number
-}
-
--- src/types/children.d.ts
+++
@@ -1,5 +0,0 @@
-import { Component, ReactElement, ReactNode } from "react"
-
-export type children = {
-    children : JSX.Element | JSX.Element[] | ReactNode
-}
